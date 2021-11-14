0ad75733170cf7e986d438f6ad889a3d1aff111a
SolodkayaSA <svetlana.solodkaia@moex.com>
2021-11-14 12:25:54 +0300

    Added task 3 The problem with entering the package was solved by removing restrictions from the file setup.py

--- test_03/setup.py
+++ test_03/setup.py
@@ -4,6 +4,5 @@ setup(
     name = 'test_project',
     author = 'SV',
     packages = ['Test_date'],
-    install_requires = ['requests'],
     entry_points = {'new_script': ['Test_date = Test_date.test_date_code:main']},
 )
